7c223abfe6d3eb726d06791d6f31b89584104f0c
luthfi <luthlirik@gmail.com>
2025-08-29 13:11:23 +0700

    kajian Page

--- server/views/kajian.nim
+++ server/views/kajian.nim
@@ -1,4 +1,28 @@
-import prologue
+import
+    prologue,
+    os,
+    nimja,
+    puppy,
+    strformat
 
 proc kajianIndex*(ctx: Context) {.async.} =
     await ctx.staticFileResponse("index.html", "src/pages/kajian")
+
+proc kelasPage*(ctx: Context) {.async.} =
+    proc myRenderProc(
+        title: string,
+        description: string): string =
+        compileTemplateFile("../../src/pages/kajian/page.html", baseDir = getScriptDir())    
+    
+    var
+        slug = ctx.getPathParams("slug")
+        ball = puppy.get(
+            fmt"http://localhost/api/kajian/meta/{slug}")
+        sukamto = parseJson ball.body
+
+    if sukamto["data"].len > 0:
+        resp myRenderProc(
+            getStr sukamto["data"]["judul"],
+            getStr sukamto["data"]["deskripsi"])        
+    else :
+        resp "<h1>404 Not Found</h1>"
