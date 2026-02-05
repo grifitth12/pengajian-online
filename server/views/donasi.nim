e2b1092605342361bdebdaec119c0c18fe288c15
upi-0 <upi@mis.my.id>
2026-02-05 11:36:44 +0700

    Add donatur inspection route and update pengumpulanDonasi page

--- server/views/donasi.nim
+++ server/views/donasi.nim
@@ -1,9 +1,29 @@
 import
     prologue,
-    withTemplate
+    withTemplate,
+    puppy,
+    json
 
 proc donasiIndex*(ctx: Context) {.async.} =
     resp generalTemplate "src/pages/donasi/index.upi"
 
 proc donasiPage*(ctx: Context) {.async.} =
     resp noTemplate "src/pages/donasi/page.upi"
+
+proc donaturInspect*(ctx: Context) {.async.} =
+    let
+        host = loadPrologueEnv(".env").get("BACKEND_URL")
+        id_program_donasi = ctx.getPathParams("id_program_donasi")
+        header = @[("Cookie", "access_token=" & ctx.getCookie("access_token"))]
+        metadata = @[(name: "id_program_donasi", content: id_program_donasi)]
+        response = get(host & "/api/donasi/" & id_program_donasi & "/donatur", header)
+
+    if response.code == 200:
+        resp loadAdminTemplate(
+            "src/pages/manage/pengumpulanDonasi.upi",
+            response.body.parseJson(),
+            metadata
+        )
+
+    else:
+        resp $response.code        
